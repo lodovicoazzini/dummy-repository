a0a7277de17b2d1cbc2397a2c016a8c5eecc7194
Lodovico Azzini <lodovico.azzini@usi.ch>
2021-12-04 19:57:46 +0100

    multiple changes

--- file2.py
+++ file2.py
@@ -2,11 +2,8 @@ class File2:
   def function1():
     return 'one'
 
-  def function2():
-    return 'twotwo'
-
   def function3():
     return 'three'
 
   def function4():
-    return 'four'
+    return 'five'
